8bfae9a0d3dd2260ed572f854a197d0f47a1e6ff
Marc A. Müller <mmueller@blauwaldt.it>
2024-05-30 16:39:13 +0200

    build

---
+++ docs/examples_textareaInserts_2cols_css.jsonEditor.js
@@ -0,0 +1,17 @@
+"use strict";
+(self["webpackChunkmsk_oc_externalpageframes"] = self["webpackChunkmsk_oc_externalpageframes"] || []).push([["examples_textareaInserts_2cols_css"],{
+
+/***/ "./examples/textareaInserts_2cols.css":
+/*!********************************************!*\
+  !*** ./examples/textareaInserts_2cols.css ***!
+  \********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZXhhbXBsZXNfdGV4dGFyZWFJbnNlcnRzXzJjb2xzX2Nzcy5qc29uRWRpdG9yLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7QUFBQSIsInNvdXJjZXMiOlsid2VicGFjazovL21zay1vYy1leHRlcm5hbHBhZ2VmcmFtZXMvLi9leGFtcGxlcy90ZXh0YXJlYUluc2VydHNfMmNvbHMuY3NzP2JhYjMiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gZXh0cmFjdGVkIGJ5IG1pbmktY3NzLWV4dHJhY3QtcGx1Z2luXG5leHBvcnQge307Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9

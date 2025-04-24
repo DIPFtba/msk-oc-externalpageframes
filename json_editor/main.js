fa3f60865e6bc29210ecd49beebac4e14fb3e6aa
Marc A. Müller <mmueller@blauwaldt.it>
2025-04-24 18:07:33 +0200

    https://gitlab.educs-hosting.net/schumas/testentwicklung-mathematik/-/issues/1876

--- json_editor/main.js
+++ json_editor/main.js
@@ -269,13 +269,13 @@ function loadSchema( schema ) {
 
 //////////////////////////////////////////////////////////////////////////////
 
-/// #if __DEVELOP
+// /// #if __DEVELOP
 
-// for Development: always load one JSON schema
-loadSchema( freePaintMultJSONSchema );
-window.updateEWK = updateEWK;
+// // for Development: always load one JSON schema
+// loadSchema( freePaintMultJSONSchema );
+// window.updateEWK = updateEWK;
 
-/// #else
+// /// #else
 
 // load schema Links
 const templs = {
@@ -314,7 +314,7 @@ Object.entries(templs).forEach( ([templ,[schema,svg]]) => {
 	schSel.appendChild( a );
 })
 
-/// #endif
+// /// #endif
 
 //////////////////////////////////////////////////////////////////////////////
 

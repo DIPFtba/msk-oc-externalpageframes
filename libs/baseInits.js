96cf80be52bccd8e547291356598d579771470a6
Marc A. Müller <mmueller@blauwaldt.it>
2025-04-26 14:52:01 +0200

    Konva Dep in Text EWKs

--- libs/baseInits.js
+++ libs/baseInits.js
@@ -1,9 +1,7 @@
 import { object_equals } from './common'
 import { fsmSend } from './fsm'
 
-import Konva from 'konva/lib/Core'
-
-// Konva should bei imported, but doens't seem to support tree shaking, so leave it out
+// Konva should bei imported, but then tree shaking isn't working, so leave it out
 // import Konva from 'konva/lib/Core'
 
 export class baseInits {

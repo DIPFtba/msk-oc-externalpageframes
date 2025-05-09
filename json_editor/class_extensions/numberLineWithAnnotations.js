a1d7ae6c2230ac7612929ef87258de9c30571316
Marc A. Müller <mmueller@blauwaldt.it>
2025-05-09 13:57:59 +0200

    numberlineWithAnnotations +createInpXVars +createConnXVars

--- json_editor/class_extensions/numberLineWithAnnotations.js
+++ json_editor/class_extensions/numberLineWithAnnotations.js
@@ -17,6 +17,9 @@ export class numberLineWithAnnotationsFromSchema extends numberLineWithAnnotatio
 
 		super( base, opts );
 
+		this.rwAnns = this.annotations.filter( a => !a.textReadonly );
+		this.rwConns = this.annotations.filter( a => !a.toValueReadonly );
+
 		addScoring( this, opts, addMods.Parser );
 
 		if ( base.fsm && base.fsm.decInitCnt ) {
@@ -33,38 +36,51 @@ export class numberLineWithAnnotationsFromSchema extends numberLineWithAnnotatio
 		const pref = settings.variablePrefix;
 		const mult = settings.xMult;
 		const scores = {};
+		if ( !( "rwAnns" in this ) ) {	// only super() initialized
+			return scores;
+		}
 
 		// Annotations
-		const anns = this.annotations.filter( a => !a.textReadonly );
-		anns.forEach( (v,i) => scores[ `V_Input_${pref}_Lab_${i+1}` ] = this.labValFnc( v.text ) );
+		this.rwAnns.forEach( (v,i) => {
+			scores[ `V_Input_${pref}_Lab_${i+1}` ] = this.labValFnc( v.text );
+			if ( settings.createInpXVars ) {
+				scores[ `V_Status_${pref}_Lab_${i+1}` ] = +( v.text.length > 0 );
+			}
+		});
+
 
 		// Connections
-		const conns = this.annotations.filter( a => !a.toValueReadonly );
-		conns.forEach( (v,i) => scores[ `V_Input_${pref}_Conn_${i+1}` ] = v.toValue === null ? null : Math.round( v.toValue * mult ) );
+		this.rwConns.forEach( (v,i) => {
+			const w = v.toValue === null ? null : Math.round( v.toValue * mult );
+			scores[ `V_Input_${pref}_Conn_${i+1}` ] = w;
+			if ( settings.createConnXVars ) {
+				scores[ `V_Status_${pref}_Conn_${i+1}` ] = +( w !== null );
+			}
+		});
 
 		// Status vars
 		let connSomeVars, connAllVars, inpSomeVars, inpAllVars;
 
 		if ( settings.createInpSomeVars || settings.createSomeVars ) {
-			inpSomeVars = anns.length>0 && anns.some( v => v.text.trim().length>0 );
+			inpSomeVars = this.rwAnns.length>0 && this.rwAnns.some( v => v.text.trim().length>0 );
 			if ( settings.createInpSomeVars ) {
 				scores[ `V_Status_${pref}_Lab_Any` ] = +inpSomeVars;
 			}
 		}
 		if ( settings.createInpAllVars || settings.createAllVars ) {
-			inpAllVars = anns.length>0 && anns.every( v => v.text.trim().length>0 );
+			inpAllVars = this.rwAnns.length>0 && this.rwAnns.every( v => v.text.trim().length>0 );
 			if ( settings.createInpAllVars ) {
 				scores[ `V_Status_${pref}_Lab_All` ] = +inpAllVars;
 			}
 		}
 		if ( settings.createConnSomeVars || settings.createSomeVars ) {
-			connSomeVars = conns.length>0 && conns.some( v => v.toValue!==null );
+			connSomeVars = this.rwConns.length>0 && this.rwConns.some( v => v.toValue!==null );
 			if ( settings.createConnSomeVars ) {
 				scores[ `V_Status_${pref}_Conn_Any` ] = +connSomeVars;
 			}
 		}
 		if ( settings.createConnAllVars || settings.createAllVars ) {
-			connAllVars = conns.length>0 && conns.every( v => v.toValue!==null );
+			connAllVars = this.rwConns.length>0 && this.rwConns.every( v => v.toValue!==null );
 			if ( settings.createConnAllVars ) {
 				scores[ `V_Status_${pref}_Conn_All` ] = +connAllVars;
 			}

18f927cd2096595d8677b92d80d0f3c18ca1fd7c
Marc A. Müller <mmueller@blauwaldt.it>
2025-12-01 16:40:52 +0100

    baseInits.d.ts

---
+++ libs/baseInits.d.ts
@@ -0,0 +1,21 @@
+//
+// Typdeklarationen, um einige funktionen auch in TapeScript projekten nutzen zu können
+//
+
+export class baseInits {
+
+    constructor( cfg={} );
+
+    postLog( event: string, data={} ): void;
+    postVariable ( name: string, val: string|number|boolean ): void;
+    triggerInputValidationEvent (): void;
+
+    getChangeState ( obj:record<string, any> ): number;
+    sendChangeState ( obj:record<string,any>, newState:record<string, any>|null = null ): void;
+
+    getInitDonePromise (): Promise<void>;
+    incInitCnt (): void;
+    decInitCnt (): void;
+    regSendInitDone (): void;
+
+}

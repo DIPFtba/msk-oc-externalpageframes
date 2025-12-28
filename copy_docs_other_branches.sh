6f696d0727a271463414a319acffb96a39ece298
Marc A. Müller <mmueller@blauwaldt.it>
2025-12-28 12:02:43 +0100

    Pages test

--- copy_docs_other_branches.sh
+++ copy_docs_other_branches.sh
@@ -4,14 +4,16 @@ function copy_branch {
     local BRANCH=$1
     local DEST_DIR="docs/${BRANCH}"
 
-    TDIR=$(mktemp -d temp_repo_XXXX)
+    TDIR=$(mktemp -d /tmp/temp_repo_XXXX)
     [[ -d "$TDIR" ]] || { echo "Failed to create temporary directory"; exit 1; }
 
+    git stash push -m "Stash before copying docs" || { echo "Failed to stash changes"; rm -rf "$TDIR"; exit 1; }
     git checkout ${BRANCH} || { echo "Failed to checkout branch ${BRANCH}"; rm -rf "$TDIR"; exit 1; }
     cp -v docs/* ${TDIR} || { echo "Failed to copy documentation to temporary directory"; rm -rf "$TDIR"; exit 1; }
     echo "Copied documentation to branch ${BRANCH} in directory ${TDIR}"
 
     git checkout jsonEditor || { echo "Failed to checkout main branch"; rm -rf "$TDIR"; exit 1; }
+    git stash pop || { echo "Failed to pop stashed changes"; rm -rf "$TDIR"; exit 1; }
     mkdir -p ${DEST_DIR} || { echo "Failed to create destination directory ${DEST_DIR}"; rm -rf "$TDIR"; exit 1; }
     cp -v ${TDIR}/* ${DEST_DIR}/ || { echo "Failed to copy documentation from temporary directory to ${DEST_DIR}"; rm -rf "$TDIR"; exit 1; }
     echo "Copied documentation from branch ${BRANCH} to ${DEST_DIR}"
--- docs/jsonEditor_Scoring_evalVarNames/895.jsonEditor.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmsk_oc_externalpageframes=self.webpackChunkmsk_oc_externalpageframes||[]).push([[895],{895:(e,s,a)=>{a.r(s)}}]);
